405edcaf270677092fc32fb5841da37787d7038b
David M Sibley <david.m.sibley@gmail.com>
2016-11-07 13:42:45 -0600

    making it fade

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("I'm here!");
+!!(function carousel() {
+	var index = 0;
+	var maxIndex = 0;
+
+	(function initIDs() {
+		console.log("init'd")
+		var imgs = document.getElementsByTagName("img");
+		for (img of imgs) {
+			img.id = maxIndex++;
+		}
+		updateTransition();
+	})();
+
+	function updateTransition() {
+		console.log("updated")
+		var activated = document.querySelector("img.activated");
+		var next = document.getElementById("" + index++);
+
+		if (index >= maxIndex) {
+			index = 0;
+		}
+
+		if (activated) {
+			activated.className = "queued";
+		}
+		if (next) {
+			next.className = "activated";
+		}
+
+		return true;
+	}
+
+	var intervalID = window.setInterval(updateTransition, 7000);
+})();
